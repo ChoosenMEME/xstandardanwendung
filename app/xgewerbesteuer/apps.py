fcacc43082432de5e76e4a36c70d01c6ed300627
ChoosenMEME <24540327+ChoosenMEME@users.noreply.github.com>
2026-06-11 12:40:22 +0200

    remove unused models

--- app/xgewerbesteuer/apps.py
+++ app/xgewerbesteuer/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
-class XgewerbesteuerConfig(AppConfig):
+class XGewerbesteuerConfig(AppConfig):
     name = 'xgewerbesteuer'
--- app/xgewerbesteuer/views.py
+++ app/xgewerbesteuer/views.py
@@ -1,8 +1,2 @@
 from django.shortcuts import render
-from .models import xgewerbesteuer
 
-# Create your views here.
-
-def xgewerbesteuer_default(request):
-    daten = xgewerbesteuer.objects.all()
-    return render(request, "xgewerbesteuer_default.html", {"daten": daten})
