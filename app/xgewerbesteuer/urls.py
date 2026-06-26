7422a1c35ab909c29ab59a3828c12c3c20e3af6b
Alexander Bahlmann <abahlmann@stud.hs-bremen.de>
2026-06-26 18:10:47 +0200

    PDF-Bericht für Bescheidauswertung ergänzen

--- app/xgewerbesteuer/urls.py
+++ app/xgewerbesteuer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import xgewerbesteuer_default
+from .views import xgewerbesteuer_default, xgewerbesteuer_pdf_report
 
 urlpatterns = [
     path("", xgewerbesteuer_default, name="xgewerbesteuer_default"),
+    path("pdf-bericht/", xgewerbesteuer_pdf_report, name="xgewerbesteuer_pdf_report"),
 ]
